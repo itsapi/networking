81937479fbd6b15375dd4b5ac2f397d93b29aecd
Geraint White <geraintwhite@gmail.com>
2013-10-28 13:05:35 +0000

    Added putKey and getKey

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ from networking.client import *
 
 class Net(object):
     def __init__(self):
-        # 
+        # Extract data from file
         try:
             f = open('data', 'r')
             data = json.load(f)
@@ -30,5 +30,33 @@ class Net(object):
 
     def putData(self, data):
         f = open('data', 'w')
+        json.dump(data, f)
+        f.close()
+
+    def getKey(self, key):        
+        f = open('data', 'r')
+        data = json.load(f)
+        f.close()
+
+        for i in data:
+            if i[0] == key:
+                break
+        if i[0] != key:
+            i[1] = False
+
+        return i[1]
+
+    def putKey(self, key, value):        
+        f = open('data', 'w')
+        data = json.load(f)
+
+        for x,i in enumerate(data):
+            if i[0] == key:
+                break
+        if i[0] == key:
+            data[x] = [key, value]
+        else:
+            data.append([key, value])
+
         json.dump(data, f)
         f.close()
